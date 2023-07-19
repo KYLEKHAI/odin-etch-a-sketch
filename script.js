f83c2a8d376f9c5f905e05cfe6281ffe0b4577bc
kylekhaitran <kylekhai04@gmail.com>
2023-07-18 23:35:56 -0400

    Add hover event t change grid color

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
-// Creating 16x16 square divs grid
-const containerDiv = document.querySelector(".container-div");
+// Load after HTML is parsed
+document.addEventListener("DOMContentLoaded", function () {
+  // Creating 16x16 square divs grid
+  const containerDiv = document.querySelector(".container-div");
 
-// Create size of a grid (16x16)
-let gridSize = 16;
+  // Create size of a grid (16x16)
+  let gridSize = 16;
 
-for (let i = 0; i < gridSize * gridSize; i++) {
-  const squareDiv = document.createElement("div");
-  squareDiv.style.backgroundColor = "MediumPurple";
+  // Creating mouse hover event
+  function mouseEnter(e) {
+    e.target.classList.add("hover");
+  }
 
-  // Assign squareDiv a class name to style via CSS
-  squareDiv.className = "square-div";
-  containerDiv.appendChild(squareDiv);
-}
+  // Creating mouse leave event
+  function mouseLeave(e) {}
 
-// Appending to document
-document.body.appendChild(containerDiv);
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    const squareDiv = document.createElement("div");
+
+    // Assign squareDiv a class name to style via CSS
+    squareDiv.className = "square-div";
+
+    // Add event listeners for mouseEnter and mouseLeave
+    squareDiv.addEventListener("mouseenter", mouseEnter);
+    squareDiv.addEventListener("mouseleave", mouseLeave);
+
+    containerDiv.appendChild(squareDiv);
+  }
+
+  // Appending to document
+  document.body.appendChild(containerDiv);
+});
